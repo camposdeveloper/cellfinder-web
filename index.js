3f8bb7690cf9e4f30507fd432eb6ea9655c96d5b
mauricio <mauriciojr.dev@gmail.com>
2019-11-12 20:57:15 -0300

    Upload
    subindo arquivos da aplicação...

---
+++ index.js
@@ -0,0 +1,28 @@
+const GoogleSpreadsheet = require('google-spreadsheet')
+const credentials = require ('./credentials.json')
+
+const { promisify } = require('util')
+const docId = '1f7UznepF4rX43WDiYfUXHnpKqEs5w_fVOuu1IZhEO8M'
+
+const accessSheet = async() =>{
+    const doc = new GoogleSpreadsheet(docId)
+    await promisify(doc.useServiceAccountAuth)(credentials)
+    const info = await promisify(doc.getInfo)()
+    const worksheet = info.worksheets[0]
+    const rows = await promisify(worksheet.getRows)({
+    })
+  
+    rows.forEach(row =>{
+      console.log(row.nome)
+  
+    })
+  
+}
+function clickButton(){
+
+  accessSheet()
+
+  
+}
+
+clickButton()
